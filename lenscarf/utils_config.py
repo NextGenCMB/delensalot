f38bfd17eb3311b47d4aa171e49e54be39bc672c
jcarron <to.jcarron@gmail.com>
2021-06-15 12:09:16 +0200

    minor

--- lenscarf/utils_config.py
+++ lenscarf/utils_config.py
@@ -12,7 +12,7 @@ def cmbs4_06b():
     pb_extent = pbounds_exl[0] + (360. - pbounds_exl[1])
     scarf_job = us.scarfjob()
     scarf_job.set_healpix_geometry(2048, zbounds=zbounds_len)
-    return scarf_job, [pb_ctr / 180 * np.pi, pb_extent/ 180 * np.pi]
+    return scarf_job, [pb_ctr / 180 * np.pi, pb_extent/ 180 * np.pi], zbounds_len
 
 def cmbs4_08b_healpix():
     zbounds_len = [-0.9736165659024625, -0.4721687661208586]
@@ -21,4 +21,4 @@ def cmbs4_08b_healpix():
     pb_extent = pbounds_exl[0] + (360. - pbounds_exl[1])
     scarf_job = us.scarfjob()
     scarf_job.set_healpix_geometry(2048, zbounds=zbounds_len)
-    return scarf_job, [pb_ctr/ 180 * np.pi, pb_extent/ 180 * np.pi]
+    return scarf_job, [pb_ctr/ 180 * np.pi, pb_extent/ 180 * np.pi], zbounds_len
