0a36c6acff0503b4a5549e6bb3daefc9fa4c9587
jcarron <to.jcarron@gmail.com>
2021-07-29 12:05:11 +0200

    misc

--- tests/par_s4P_fs.py
+++ tests/par_s4P_fs.py
@@ -65,17 +65,20 @@ chain_descr = [[0, ["diag_cl"], lmax_unl, 2048, np.inf, 1e-3, cd_solve.tr_cg, cd
 def get_itlib(vscarf):
     tr = int(os.environ.get('OMP_NUM_THREADS', 8))
     cpp = cls_unl['pp'][:lmax_qlm + 1]
+
+    sc_job = utils_scarf.scarfjob()
+    sc_job.set_thingauss_geometry(lmax_unl, 2)
+    d_geo = utils_scarf.pbdGeometry(sc_job.geom, utils_scarf.pbounds(pb_ctr, pb_extent))
+
     if not os.path.exists(TEMP + '/plm0.npy'):
         if not os.path.exists(TEMP + '/eblm_dat.npy'):
             eblm = build_sim(0, transf, nlev_p)
             np.save(TEMP + '/eblm_dat.npy', eblm)
-        eblm_wf = np.load(TEMP + '/eblm_dat.npy')
+        eblm = np.load(TEMP + '/eblm_dat.npy')
+        eblm_wf = np.copy(eblm)
         almxfl(eblm_wf[0], transf_i * fel * cls_len['ee'][:lmax + 1], mmax, inplace=True)
         almxfl(eblm_wf[1], transf_i * fbl * cls_len['bb'][:lmax + 1], mmax, inplace=True)
 
-        sc_job = utils_scarf.scarfjob()
-        sc_job.set_thingauss_geometry(lmax_unl, 2)
-        d_geo = utils_scarf.pbdGeometry(sc_job.geom, utils_scarf.pbounds(pb_ctr, pb_extent))
         isoppfilter = alm_filter_nlev(nlev_p, transf, (lmax, lmax))
         plm0 = isoppfilter.get_qlms(eblm, eblm_wf, d_geo, lmax_qlm, lmax_qlm)[0]
         R = qresp.get_response('p_p', lmax, 'p', cls_len, cls_len, {'e': fel, 'b': fbl}, lmax_qlm=lmax_qlm)[0]
@@ -91,7 +94,7 @@ def get_itlib(vscarf):
     isofilter = alm_filter_nlev_wl(nlev_p, ffi, transf, (lmax_unl, lmax_unl), (lmax, lmax))
     stepper = steps.nrstep(lmax_qlm, mmax_qlm, val=0.5)
     k_geom = isofilter.ffi.geom
-    itlib = scarf_iterator.iterator_cstmf( TEMP + '/s4iterator_fullsky_p_p_%04d'%idx + args.scarf, vscarf[0], (lmax_qlm, mmax_qlm), eblm,
+    itlib = scarf_iterator.iterator_cstmf( TEMP + '/s4iterator_fullsky_p_p_%04d'%idx + args.scarf, vscarf[0], (lmax_qlm, mmax_qlm), np.load(TEMP + '/eblm_dat.npy'),
             plm0, plm0 * 0., H0_unl, cpp, cls_unl, isofilter, k_geom, chain_descr, stepper)
     return itlib
 
