59067d56127bfd07e78cad65c6ee5c4f0e45fb5e
Sebastian-Belkner <sebastian.belkner@unige.ch>
2022-05-22 04:56:44 -0700

    update

--- scripts/build_OBD.py
+++ scripts/build_OBD.py
@@ -1,22 +1,20 @@
 """Builds OBD matrix for polarisation CMB-S4 08d
 adapted from PL2018.
+
+Note:
+    It takes about 60min to calculate all rows when using 2 nodes with -c 4, for bmarg_lmax=200,
+    30min to collect, and 60min to store    
 """
 
 import os
 import numpy as np
 import healpy as hp
 
-import lenscarf
 from lenscarf import utils_scarf
 from lenscarf.utils import read_map
-from lenscarf import utils
 from lenscarf.opfilt import bmodes_ninv
 
 from plancklens.helpers import mpi
-from plancklens.filt import  filt_cinv
-
-
-# from lerepi.params.s08d import s08d as parfile
 
 
 lmax_marg = 200  # max marged multipole
@@ -45,7 +43,7 @@ if __name__ == '__main__':
     # This scripts uses a number of processes to calculate all rows of the dense OBD matrix
     # After calculting the rows, the rows are collected by the first process, who then regularize it,  inverse it,
     # and cache the inverse under the name tniti.npy in the specified folder
-    TEMP =  '/global/cscratch1/sd/sebibel/cmbs4/s08d/OBDmatrix_test1/'
+    TEMP =  '/global/cscratch1/sd/sebibel/cmbs4/s08d/OBDmatrix/'
     libdir = os.path.join(TEMP, 'OBD')
 
 
@@ -54,8 +52,9 @@ if __name__ == '__main__':
 
         """
         Find ninv_p
+        THIS NEEDS TO BE CHANGED FOR A NEW SIM DATA SET
         """
-        centralnoiselevel = 1.
+        centralnoiselevel = .59
         rhits = np.nan_to_num(hp.read_map('/project/projectdirs/cmbs4/awg/lowellbb/expt_xx/08d/rhits/n2048.fits'))
         pixlev = centralnoiselevel / (np.sqrt(hp.nside2pixarea(2048, degrees=True)) * 60.)
         print("Pmap center pixel pol noise level: %.2f"%(pixlev * np.sqrt(hp.nside2pixarea(nside, degrees=True)) * 60.))
@@ -67,13 +66,13 @@ if __name__ == '__main__':
             os.makedirs(libdir)
         mpi.barrier()
 
-        NiT = read_map(ninv_p)  # inverse pixel variance map
+        NiT = ninv_p  #NiT = read_map(ninv_p) # inverse pixel variance map
         bpl = bmodes_ninv.template_bfilt(lmax_marg, geom, int(os.environ.get('OMP_NUM_THREADS', 4)), _lib_dir=libdir)
         bpl._get_rows_mpi(NiT, prefix)  # builds all rows in parallel
         mpi.barrier()
         if mpi.rank == 0:
             assert not os.path.exists(bpl.lib_dir + '/tniti.npy')
-            tnit = bpl._build_tnit()
+            tnit = bpl._build_tniti()
             nlevp = 10000.  # regularization, saying the modes have in fact huge noise
             tniti = np.linalg.inv(tnit + np.diag((1. / (nlevp / 180. / 60. * np.pi) ** 2) * np.ones(tnit.shape[0])))
             np.save(bpl.lib_dir + '/tniti.npy', tniti)
