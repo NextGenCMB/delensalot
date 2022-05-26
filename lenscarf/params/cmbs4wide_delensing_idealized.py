209739fdf314069186fa8b05f0c4e3918fbcb12c
jcarron <to.jcarron@gmail.com>
2022-05-26 20:39:40 +0200

    minors

--- lenscarf/params/cmbs4wide_delensing_idealized.py
+++ lenscarf/params/cmbs4wide_delensing_idealized.py
@@ -175,7 +175,6 @@ def get_itlib(k:str, simidx:int, version:str, cg_tol:float):
     ffi = remapping.deflection(lenjob_pbgeometry, lensres, np.zeros_like(plm0), mmax_qlm, tr, tr)
     if k in ['p_p', 'p_eb']:
         wee = k == 'p_p' # keeps or not the EE-like terms in the generalized QEs
-        assert np.all(transf_elm == transf_blm), 'This is not supported by the alm_filter_nlev_wl (but easy to fix)'
         # Here multipole cuts are set by the transfer function (those with 0 are not considered)
         filtr = alm_filter_nlev_wl(nlev_p, ffi, transf_elm, (lmax_unl, mmax_unl), (lmax_ivf, mmax_ivf),
                                    wee=wee, transf_b=transf_blm, nlev_b=nlev_p)
