49a86f175700ae082e5213e152a319a560943f1e
Sebastian-Belkner <sebastian.belkner@unige.ch>
2022-05-31 18:01:06 -0700

    cleanup

--- lenscarf/core/handler.py
+++ lenscarf/core/handler.py
@@ -1,49 +1,113 @@
-import os
+#!/usr/bin/env python
+
+"""handler.py: This module receives input from lerepi.
+    
+"""
+__author__ = "S. Belkner, J. Carron, L. Legrand"
+
+
 from os.path import join as opj
+import logging
+from logdecorator import log_on_start, log_on_end
 
 import numpy as np
 
+from plancklens import utils, qresp
 from plancklens.helpers import mpi
 
+from lenscarf.utils_hp import almxfl, alm_copy
 from lenscarf.iterators.statics import rec as Rec
 from lenscarf.iterators import iteration_handler
 
 
 class MAP_delensing():
-    def __init__(self, survey_config, lensing_config, run_config):
-        self.survey_config = survey_config
+    def __init__(self, qe, lensing_config):
+        self.qe = qe
         self.lensing_config = lensing_config
-        self.run_config = run_config
-        self.libdir_iterators = lambda qe_key, simidx, version: opj(run_config.TEMP,'%s_sim%04d'%(qe_key, simidx) + version)
+        self.libdir_iterators = lambda qe_key, simidx, version: opj(lensing_config.TEMP,'%s_sim%04d'%(qe_key, simidx) + version)
         self.ith = iteration_handler.transformer(lensing_config.iterator)
 
 
+    @log_on_start(logging.INFO, "Start of collect_jobs()")
+    @log_on_end(logging.INFO, "Finished collect_jobs()")
     def collect_jobs(self):
         jobs = []
-        for idx in np.arange(self.run_config.imin, self.run_config.imax + 1):
-            lib_dir_iterator = self.libdir_iterators(self.run_config.k, idx, self.run_config.v)
-            if Rec.maxiterdone(lib_dir_iterator) < self.run_config.itmax:
+        for idx in np.arange(self.lensing_config.imin, self.lensing_config.imax + 1):
+            lib_dir_iterator = self.libdir_iterators(self.lensing_config.k, idx, self.lensing_config.v)
+            if Rec.maxiterdone(lib_dir_iterator) < self.lensing_config.itmax:
                 jobs.append(idx)
         self.jobs = jobs
 
 
+    @log_on_start(logging.INFO, "Start of run()")
+    @log_on_end(logging.INFO, "Finished run()")
     def run(self):
         for idx in self.jobs[mpi.rank::mpi.size]:
-            lib_dir_iterator = self.libdir_iterators(self.run_config.k, idx, self.run_config.v)
-            if self.run_config.itmax >= 0 and Rec.maxiterdone(lib_dir_iterator) < self.run_config.itmax:
-                itlib = self.ith(self.run_config.k, idx, self.run_config.v, self.libdir_iterators, self.lensing_config, self.survey_config, self.run_config)
+            lib_dir_iterator = self.libdir_iterators(self.lensing_config.k, idx, self.lensing_config.v)
+            if self.lensing_config.itmax >= 0 and Rec.maxiterdone(lib_dir_iterator) < self.lensing_config.itmax:
+                itlib = self.ith(self.qe, self.lensing_config.k, idx, self.lensing_config.v, self.libdir_iterators, self.lensing_config, self.lensing_config, self.lensing_config)
                 itlib_iterator = itlib.get_iterator()
-                for i in range(self.run_config.itmax + 1):
+                for i in range(self.lensing_config.itmax + 1):
                     # print("Rank {} with size {} is starting iteration {}".format(mpi.rank, mpi.size, i))
-                    print("****Iterator: setting cg-tol to %.4e ****"%self.run_config.tol_iter(i))
-                    print("****Iterator: setting solcond to %s ****"%self.run_config.soltn_cond(i))
-                    itlib_iterator.chain_descr  = self.lensing_config.chain_descr(self.lensing_config.lmax_unl, self.run_config.tol_iter(i))
-                    itlib_iterator.soltn_cond   = self.run_config.soltn_cond(i)
+                    print("****Iterator: setting cg-tol to %.4e ****"%self.lensing_config.tol_iter(i))
+                    print("****Iterator: setting solcond to %s ****"%self.lensing_config.soltn_cond(i))
+                    itlib_iterator.chain_descr  = self.lensing_config.chain_descr(self.lensing_config.lmax_unl, self.lensing_config.tol_iter(i))
+                    itlib_iterator.soltn_cond   = self.lensing_config.soltn_cond(i)
                     print("doing iter " + str(i))
                     itlib_iterator.iterate(i, 'p')
 
 
 class QE_delensing():
-    def __init__(self, survey_config, lensing_config, run_config):
-        assert 0, "Implement if needed"
-        pass
+    def __init__(self, lensing_config):
+        self.lensing_config = lensing_config
+        self.mf0 = self.get_meanfield_it0()
+        self.plm0 = self.get_plm_it0()
+        self.mf_resp = self.get_meanfield_response_it0()
+        self.wflm0 = lambda simidx: alm_copy(lensing_config.ivfs.get_sim_emliklm(simidx), None, lensing_config.lmax_unl, lensing_config.mmax_unl)
+        self.R_unl = qresp.get_response(self.k, self.lensing_config.lmax_ivf, 'p', self.lensing_config.cls_unl, self.lensing_config.cls_unl,  {'e': self.lensing_config.fel_unl, 'b': self.lensing_config.fbl_unl, 't':self.lensing_config.ftl_unl}, lmax_qlm=self.lensing_config.lmax_qlm)[0]
+
+
+    @log_on_start(logging.INFO, "Start of get_meanfield_it0()")
+    @log_on_end(logging.INFO, "Finished get_meanfield_it0()")
+    def get_meanfield_it0(self):
+        # QE mean-field fed in as constant piece in the iteration steps:
+        mf_sims = np.unique(self.lensing_config.mc_sims_mf_it0 if not 'noMF' in self.version else np.array([]))
+        mf0 = self.lensing_config.qlms_dd.get_sim_qlm_mf(self.k, mf_sims)  # Mean-field to subtract on the first iteration:
+        if self.simidx in mf_sims:  # We dont want to include the sim we consider in the mean-field...
+            Nmf = len(mf_sims)
+            mf0 = (mf0 - self.lensing_config.qlms_dd.get_sim_qlm(self.k, int(self.simidx)) / Nmf) * (Nmf / (Nmf - 1))
+        
+        return mf0
+
+
+    @log_on_start(logging.INFO, "Start of get_plm_it0()")
+    @log_on_end(logging.INFO, "Finished get_plm_it0()")
+    def get_plm_it0(self):
+        path_plm0 = opj(self.libdir_iterator, 'phi_plm_it000.npy')
+        if not os.path.exists(path_plm0):
+            # We now build the Wiener-filtered QE here since not done already
+            plm0  = self.lensing_config.qlms_dd.get_sim_qlm(self.k, int(self.simidx))  #Unormalized quadratic estimate:
+            plm0 -= self.mf0  # MF-subtracted unnormalized QE
+            # Isotropic normalization of the QE
+            R = qresp.get_response(self.k, self.lensing_config.lmax_ivf, 'p', self.lensing_config.cls_len, self.lensing_config.cls_len, {'e': self.lensing_config.fel, 'b': self.lensing_config.fbl, 't':self.lensing_config.ftl}, lmax_qlm=self.lensing_config.lmax_qlm)[0]
+            # Isotropic Wiener-filter (here assuming for simplicity N0 ~ 1/R)
+            WF = self.lensing_config.cpp * utils.cli(self.lensing_config.cpp + utils.cli(R))
+            plm0 = alm_copy(plm0,  None, self.lensing_config.lmax_qlm, self.lensing_config.mmax_qlm) # Just in case the QE and MAP mmax'es were not consistent
+            almxfl(plm0, utils.cli(R), self.lensing_config.mmax_qlm, True) # Normalized QE
+            almxfl(plm0, WF, self.lensing_config.mmax_qlm, True)           # Wiener-filter QE
+            almxfl(plm0, self.lensing_config.cpp > 0, self.lensing_config.mmax_qlm, True)
+            np.save(path_plm0, plm0)
+
+        return np.load(path_plm0)
+
+
+    @log_on_start(logging.INFO, "Start of get_meanfield_response_it0()")
+    @log_on_end(logging.INFO, "Finished get_meanfield_response_it0()")
+    def get_meanfield_response_it0(self):
+        if self.k in ['p_p'] and not 'noRespMF' in self.version :
+            mf_resp = qresp.get_mf_resp(self.k, self.lensing_config.cls_unl, {'ee': self.lensing_config.fel_unl, 'bb': self.lensing_config.fbl_unl}, self.lensing_config.lmax_ivf, self.lensing_config.lmax_qlm)[0]
+        else:
+            print('*** mf_resp not implemented for key ' + self.k, ', setting it to zero')
+            mf_resp = np.zeros(self.lensing_config.lmax_qlm + 1, dtype=float)
+
+        return mf_resp
--- lenscarf/iterators/iteration_handler.py
+++ lenscarf/iterators/iteration_handler.py
@@ -3,18 +3,20 @@ from os.path import join as opj
 
 import numpy as np
 
-from plancklens import utils, qresp
-
 from lenscarf import remapping
 from lenscarf import utils_sims
-from lenscarf.utils_hp import almxfl, alm_copy
+
 from lenscarf.iterators import cs_iterator
 from lenscarf.utils import read_map
 from lenscarf.opfilt import opfilt_ee_wl
 
+# TODO not sure if I need something like this at the moment. Implement visitor pattern if needed.
+# from lenscarf.metamodel.iterator_pert import IT_PERT
+# from lenscarf.core.visitor import transform
+
 
 class scarf_iterator_pertmf():
-    def __init__(self, k:str, simidx:int, version:str, libdir_iterators, lensing_config, survey_config, run_config):
+    def __init__(self, qe, k:str, simidx:int, version:str, libdir_iterators, lensing_config):
         """Return iterator instance for simulation idx and qe_key type k
 
             Args:
@@ -28,72 +30,31 @@ class scarf_iterator_pertmf():
         self.k = k
         self.simidx = simidx
         self.version = version
-        libdir_iterator = libdir_iterators(k, simidx, version)
         self.lensing_config = lensing_config
-        self.survey_config = survey_config
-        self.libdir_iterator = libdir_iterator
+        
+        self.libdir_iterator = libdir_iterators(k, simidx, version)
         self.tpl = lensing_config.tpl
         self.tr = lensing_config.tr
-        if not os.path.exists(libdir_iterator):
-            os.makedirs(libdir_iterator)
-        print('Starting get itlib for {}'.format(libdir_iterator))
 
-        self.wflm0 = lambda : alm_copy(lensing_config.ivfs.get_sim_emliklm(simidx), None, lensing_config.lmax_unl, lensing_config.mmax_unl)
-        self.R_unl = qresp.get_response(self.k, self.lensing_config.lmax_ivf, 'p', self.lensing_config.cls_unl, self.lensing_config.cls_unl,  {'e': self.lensing_config.fel_unl, 'b': self.lensing_config.fbl_unl, 't':self.lensing_config.ftl_unl}, lmax_qlm=self.lensing_config.lmax_qlm)[0]
-        self.mf0 = self.get_meanfield_it0()
-        self.plm0 = self.get_plm_it0()
-        self.mf_resp = self.get_meanfield_response_it0()
-        self.datmaps = self.get_datmaps()
+        self.mf0 = qe.mf0
+        self.plm0 = qe.plm0
+        self.mf_resp = qe.mf_resp
+        self.wflm0 = qe.wflm0(simidx)
+        self.R_unl = qe.R_unl
+
+        if not os.path.exists(self.libdir_iterator):
+            os.makedirs(self.libdir_iterator)
+        print('Starting get itlib for {}'.format(self.libdir_iterator))
+
+        self.datmaps = self.get_datmaps() #TODO this should be done earlier. Perhaps in delensing_interface
         self.ffi = remapping.deflection(lensing_config.lenjob_pbgeometry, lensing_config.lensres, np.zeros_like(self.plm0), lensing_config.mmax_qlm, self.tr, self.tr)
-
         self.filter = self.get_filter(self.sims_MAP, self.ffi, self.tpl)
-
-
-        self.chain_descr = lensing_config.chain_descr(lensing_config.lmax_unl, run_config.cg_tol)
-
-
-    def get_meanfield_it0(self):
-        # QE mean-field fed in as constant piece in the iteration steps:
-        mf_sims = np.unique(self.lensing_config.mc_sims_mf_it0 if not 'noMF' in self.version else np.array([]))
-        mf0 = self.lensing_config.qlms_dd.get_sim_qlm_mf(self.k, mf_sims)  # Mean-field to subtract on the first iteration:
-        if self.simidx in mf_sims:  # We dont want to include the sim we consider in the mean-field...
-            Nmf = len(mf_sims)
-            mf0 = (mf0 - self.lensing_config.qlms_dd.get_sim_qlm(self.k, int(self.simidx)) / Nmf) * (Nmf / (Nmf - 1))
-        return mf0
-
-
-    def get_plm_it0(self):
-        path_plm0 = opj(self.libdir_iterator, 'phi_plm_it000.npy')
-        if not os.path.exists(path_plm0):
-            # We now build the Wiener-filtered QE here since not done already
-            plm0  = self.lensing_config.qlms_dd.get_sim_qlm(self.k, int(self.simidx))  #Unormalized quadratic estimate:
-            plm0 -= self.mf0  # MF-subtracted unnormalized QE
-            # Isotropic normalization of the QE
-            R = qresp.get_response(self.k, self.lensing_config.lmax_ivf, 'p', self.lensing_config.cls_len, self.lensing_config.cls_len, {'e': self.lensing_config.fel, 'b': self.lensing_config.fbl, 't':self.lensing_config.ftl}, lmax_qlm=self.lensing_config.lmax_qlm)[0]
-            # Isotropic Wiener-filter (here assuming for simplicity N0 ~ 1/R)
-            WF = self.lensing_config.cpp * utils.cli(self.lensing_config.cpp + utils.cli(R))
-            plm0 = alm_copy(plm0,  None, self.lensing_config.lmax_qlm, self.lensing_config.mmax_qlm) # Just in case the QE and MAP mmax'es were not consistent
-            almxfl(plm0, utils.cli(R), self.lensing_config.mmax_qlm, True) # Normalized QE
-            almxfl(plm0, WF, self.lensing_config.mmax_qlm, True)           # Wiener-filter QE
-            almxfl(plm0, self.lensing_config.cpp > 0, self.lensing_config.mmax_qlm, True)
-            np.save(path_plm0, plm0)
-
-        return np.load(path_plm0)
-
-
-    def get_meanfield_response_it0(self):
-        if self.k in ['p_p'] and not 'noRespMF' in self.version :
-            mf_resp = qresp.get_mf_resp(self.k, self.lensing_config.cls_unl, {'ee': self.lensing_config.fel_unl, 'bb': self.lensing_config.fbl_unl}, self.lensing_config.lmax_ivf, self.lensing_config.lmax_qlm)[0]
-        else:
-            print('*** mf_resp not implemented for key ' + self.k, ', setting it to zero')
-            mf_resp = np.zeros(self.lensing_config.lmax_qlm + 1, dtype=float)
-
-        return mf_resp
+        self.chain_descr = lensing_config.chain_descr(lensing_config.lmax_unl, lensing_config.cg_tol)
 
 
     def get_datmaps(self):
         assert self.k in ['p_p', 'p_eb'], '{} not supported. Implement if needed'.format(self.k)
-        self.sims_MAP  = utils_sims.ztrunc_sims(self.survey_config.sims, self.lensing_config.nside, [self.lensing_config.zbounds])
+        self.sims_MAP  = utils_sims.ztrunc_sims(self.lensing_config.sims, self.lensing_config.nside, [self.lensing_config.zbounds])
         datmaps = np.array(self.sims_MAP.get_sim_pmap(int(self.simidx)))
 
         return datmaps
@@ -129,3 +90,8 @@ def transformer(descr):
         return scarf_iterator_pertmf
     else:
         assert 0, "Not yet implemented"
+
+# TODO Above could be changed into a proper visitor pattern if needed at some point
+# @transform.case(IBN_Model, IBN2Pomegranate_Transformer)
+# def f1(expr, transformer): # pylint: disable=missing-function-docstring
+#     return transformer.pom_ibn(expr)
