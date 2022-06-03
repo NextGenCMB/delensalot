7d488651a03176df7b41fefceefbbdaa90203003
Sebastian-Belkner <sebastian.belkner@unige.ch>
2022-06-03 14:38:27 -0700

    add

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 25
+#SBATCH -C haswell
+#SBATCH -q regular
+#SBATCH -J D.lensalot
+#SBATCH --mail-user=sebastian.belkner@unige.ch
+#SBATCH --mail-type=ALL
+#SBATCH -t 02:00:00
+
+#OpenMP settings:
+export OMP_NUM_THREADS=8
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+
+#run the application:
+srun -n 100 -c 16 --cpu_bind=cores global/homes/s/sebibel/git/lerepi/run.py
