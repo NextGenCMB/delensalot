3ad9d0ab63b4492e0f3341bf4033c2b960701a21
Sebastian-Belkner <sebastian.belkner@unige.ch>
2022-06-19 17:51:47 -0700

    for Louis

--- lerepi/config/examples/example_cmbs4wide_maskpoles.py
+++ lerepi/config/examples/example_cmbs4wide_maskpoles.py
@@ -1,11 +1,7 @@
-import os
-from os.path import join as opj
 import numpy as np
 import healpy as hp
 
-from lerepi.core.metamodel.dlensalot_v2 import *
-
-from plancklens.sims import phas, planck2018_sims
+from lerepi.core.metamodel.dlensalot import *
 
 
 dlensalot_model = DLENSALOT_Model(
@@ -15,102 +11,104 @@ dlensalot_model = DLENSALOT_Model(
         MAP_lensrec = True,
         Btemplate_per_iteration = True,
         map_delensing = True,
-        inspect_result = False,
-        OMP_NUM_THREADS = 16
+        inspect_result = False
     ),
-    analysis = DLENSALOT_Analysis(
-        TEMP_suffix = '',
+    data = DLENSALOT_Data(
+        TEMP_suffix = 'r10_tol4_maxzlen',
+        fg = '00',
+        sims = 'cmbs4/08d/ILC_May2022', 
+        nside = 2048,
+        BEAM = 2.3,
+        lmax_transf = 4000,
+        transf = hp.gauss_beam,
+        tpl = 'template_dense'
+    ),
+    iteration = DLENSALOT_Iteration(
         K = 'p_p',
-        V = 'mf07',
-        ITMAX = 12,
-        nsims_mf = 100,
-        zbounds =  ('nmr_relative', np.inf),
-        zbounds_len = ('extend', 5.),   
-        pbounds = [1.97, 5.71],
-        LENSRES = 1.7, # Deflection operations will be performed at this resolution
+        V = '', 
+        ITMAX = 10,
+        IMIN = 0,
+        IMAX = 19,
+        nsims_mf = 20,
+        OMP_NUM_THREADS = 16,
         Lmin = 4, 
+        CG_TOL = 2*1e-4,
+        TOL = 4,
+        soltn_cond = lambda it: True,
         lmax_filt = 4000,
+        lmax_qlm = 4000,
+        mmax_qlm = 4000,
         lmax_unl = 4000,
         mmax_unl = 4000,
         lmax_ivf = 3000,
         mmax_ivf = 3000,
         lmin_ivf = 10,
         mmin_ivf = 10,
-        STANDARD_TRANSFERFUNCTION = True # Change only if exotic transferfunctions is desired
+        LENSRES = 1.7, # Deflection operations will be performed at this resolution
+        QE_LENSING_CL_ANALYSIS = False, # Change the following block only if a full, Planck-like QE lensing power spectrum analysis is desired
+        STANDARD_TRANSFERFUNCTION = True, # Change the following block only if exotic transferfunctions are desired
+        FILTER = 'cinv_sepTP', # Change the following block only if other than cinv_t, cinv_p, ivfs filters are desired
+        FILTER_QE = 'sepTP', # Change the following block only if other than sepTP for QE is desired
+        ITERATOR = 'pertmf' # Choose your iterator. Either pertmf or const_mf
     ),
-    data = DLENSALOT_Data(
+    geometry = DLENSALOT_Geometry(
+        lmax_unl = 4000,
+        zbounds = ('nmr_relative', 10),
+        zbounds_len = ('max', 5.),
+        pbounds = [0, 2*np.pi],
+        nside = 2048,
+        lenjob_geometry = 'thin_gauss',
+        lenjob_pbgeometry = 'pbdGeometry',
+        ninvjob_geometry = 'healpix_geometry',
+        ninvjob_qe_geometry = 'healpix_geometry_qe'
+    ),
+    chain_descriptor = DLENSALOT_Chaindescriptor(
+        p0 = 0,
+        p1 = ["diag_cl"],
+        p2 = None,
+        p3 = 2048,
+        p4 = np.inf,
+        p5 = None,
+        p6 = 'tr_cg',
+        p7 = 'cache_mem'
+    ),
+    stepper = DLENSALOT_Stepper(
+        typ = 'harmonicbump',
+        lmax_qlm = 4000,
+        mmax_qlm = 4000,
+        xa = 400,
+        xb = 1500
+    ),
+    map_delensing = DLENSALOT_Mapdelensing(
+        edges = 'cmbs4',
         IMIN = 0,
-        IMAX = 99,
-        package_ = 'plancklens',
-        module_ = 'sims.maps',
-        class_ = 'cmb_maps_nlev',
-        class_parameters = {
-            'sims_cmb_len': planck2018_sims.cmb_len_ffp10(),
-            'cl_transf': hp.gauss_beam(1.0 / 180 / 60 * np.pi, lmax=4096),
-            'nlev_t': 0.5/np.sqrt(2),
-            'nlev_p': 0.5,
-            'nside': 2048,
-            'pix_lib_phas': phas.pix_lib_phas(opj(os.environ['HOME'], 'pixphas_nside2048'), 3, (hp.nside2npix(2048),))
-        },
-        beam = 1.0,
+        IMAX = 19,
+        droplist = np.array([]),
+        ITMAX = [8, 10],
+        fg = '00',
+        base_mask = 'cmbs4/08d/ILC_May2022', # This mask is used to rotate ILC maps
+        nlevels = [2, 5, 10],
+        nside = 2048,
+        lmax_cl = 2048,
+        beam = 2.3,
         lmax_transf = 4000,
-        nside = 2048
+        transf = 'gauss',
+        Cl_fid = 'ffp10'
     ),
     noisemodel = DLENSALOT_Noisemodel(
         typ = 'OBD',
-        BMARG_LIBDIR = '/global/project/projectdirs/cmbs4/awg/lowellbb/reanalysis/mapphi_intermediate/s08b/',
+        BMARG_LIBDIR = '/global/cscratch1/sd/sebibel/cmbs4/OBD_matrices/r10', # '/global/project/projectdirs/cmbs4/awg/lowellbb/reanalysis/mapphi_intermediate/s08d/',
         BMARG_LCUT = 200,
-        BMARG_RESCALE = (0.42/0.350500)**2,
-        ninvjob_geometry = 'healpix_geometry',
+        BMARG_RESCALE = 1.0, # (0.65/0.59)**2,
         lmin_tlm = 30,
         lmin_elm = 30,
-        lmin_blm = 30,
-        CENTRALNLEV_UKAMIN = 0.42,
-        nlev_t = 0.42/np.sqrt(2),
-        nlev_p = 0.42,
-        nlev_dep = 10000.,
-        inf = 1e4,
-        mask = ('nlev', np.inf),
-        rhits_normalised = ('/global/project/projectdirs/cmbs4/awg/lowellbb/reanalysis/mapphi_intermediate/s08b/masks/08b_rhits_positive_nonan.fits', np.inf),
-        tpl = 'template_dense'
-    ),
-    qerec = DLENSALOT_Qerec(
-        FILTER_QE = 'sepTP', # Change only if other than sepTP for QE is desired
-        CG_TOL = 1e-3,
-        ninvjob_qe_geometry = 'healpix_geometry_qe',
-        lmax_qlm = 4000,
-        mmax_qlm = 4000,
-        QE_LENSING_CL_ANALYSIS = False, # Change only if a full, Planck-like QE lensing power spectrum analysis is desired
-        chain = DLENSALOT_Chaindescriptor(
-            p0 = 0,
-            p1 = ["diag_cl"],
-            p2 = None,
-            p3 = 2048,
-            p4 = np.inf,
-            p5 = None,
-            p6 = 'tr_cg',
-            p7 = 'cache_mem'
-        )
-    ),
-    itrec = DLENSALOT_Itrec(
-        FILTER = 'opfilt_ee_wl.alm_filter_ninv_wl',
-        TOL = 3,
-        lenjob_geometry = 'thin_gauss',
-        lenjob_pbgeometry = 'pbdGeometry',
-        iterator_typ = 'constmf', # Either pertmf or const_mf
-        mfvar = '/global/cscratch1/sd/sebibel/cmbs4/08b_00_OBD_MF100_example/qlms_dd/simMF_k1p_p_135b0ca72339ac4eb092666cd7acb262a8ea2d30.fits',
-        soltn_cond = lambda it: True,
-        stepper = DLENSALOT_Stepper(
-            typ = 'harmonicbump',
-            xa = 400,
-            xb = 1500
-        )
-    ),
-    madel = DLENSALOT_Mapdelensing(
-        edges = 'ioreco',
-        ITMAX = [10,12],
-        droplist = np.array([]),
-        nlevels = [2, 5],
-        lmax_cl = 2048,
+        lmin_blm = 200,
+        CENTRALNLEV_UKAMIN = 0.59,
+        nlev_t = 0.59/np.sqrt(2),
+        nlev_p = 0.59,
+        nlev_dep = 1e5,
+        inf = 1e8,
+        mask = ('nlev', 10),
+        rhits_normalised = ('/global/project/projectdirs/cmbs4/awg/lowellbb/reanalysis/mapphi_intermediate/s08d/masks/08d_rhits_positive_nonan.fits',10)
     )
 )
