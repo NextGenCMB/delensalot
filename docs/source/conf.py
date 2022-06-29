5849ab1fb5b6f865c17e4fe2f9fceeacc7bff2b1
Sebastian-Belkner <sebastian.belkner@unige.ch>
2022-06-29 16:19:17 -0700

    trying getting latex to run

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,10 +44,32 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
     "sphinx.ext.graphviz",
+    'sphinx.ext.mathjax'
 ]
 
+
+# mathjax settings
+mathjax_config ={
+    'extensions': ['tex2jax.js'],
+    'tex2jax': {
+        'inlineMath': [["$", "$"], ["\\(", "\\)"]],
+        'displayMath': [['$$', '$$'], ["\\[", "\\]"]],
+        'processEscapes': True
+    },
+    'HTML-CSS': {'fonts': ['TeX']} }
+
+# Napoleon settings
 napoleon_google_docstring = True
-# napoleon_numpy_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
