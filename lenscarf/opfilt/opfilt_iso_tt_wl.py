ae2fa202010d3caaf7ed779d67b6168ac53b96d0
jcarron <to.jcarron@gmail.com>
2023-03-01 15:31:23 +0100

    T rescaling options

--- lenscarf/opfilt/opfilt_iso_tt_wl.py
+++ lenscarf/opfilt/opfilt_iso_tt_wl.py
@@ -3,7 +3,7 @@
 
 """
 import numpy as np
-from lenscarf.utils_hp import almxfl, Alm
+from lenscarf.utils_hp import almxfl, Alm, default_rng, synalm
 from lenscarf.utils import timer, clhash
 from lenscarf import utils_scarf, remapping
 from lenscarf.opfilt import opfilt_iso_tt, opfilt_base
@@ -13,6 +13,7 @@ fwd_op = opfilt_iso_tt.fwd_op
 dot_op = opfilt_iso_tt.dot_op
 pre_op_diag = opfilt_iso_tt.pre_op_diag
 pre_op_dense = None # not implemented
+apply_fini = opfilt_iso_tt.apply_fini
 
 def _extend_cl(cl, lmax):
     """Forces input to an array of size lmax + 1
@@ -24,17 +25,9 @@ def _extend_cl(cl, lmax):
     ret[:min(len(cl), lmax+1)]= np.copy(cl[:min(len(cl), lmax+1)])
     return ret
 
-def apply_fini(*args, **kwargs):
-    """cg-inversion post-operation
-
-        If nothing output is Wiener-filtered CMB
-
-
-    """
-    pass
 
 class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
-    def __init__(self, nlev_t:float or np.ndarray, ffi:remapping.deflection, transf:np.ndarray, unlalm_info:tuple, lenalm_info:tuple, verbose=False):
+    def __init__(self, nlev_t:float or np.ndarray, ffi:remapping.deflection, transf:np.ndarray, unlalm_info:tuple, lenalm_info:tuple, verbose=False, rescal=None):
         r"""Version of alm_filter_ninv_wl for full-sky maps filtered with homogeneous noise levels
 
 
@@ -44,7 +37,7 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
                     transf: transfer function (beam, pixel window, mutlipole cuts, ...)
                     unlalm_info: lmax and mmax of unlensed CMB
                     lenalm_info: lmax and mmax of lensed CMB (greater or equal the transfer lmax)
-
+                    rescal: the WF will search for the sol of Tlm * rescal(l) if set (should not change anything)
 
                 Note:
                     All operations are in harmonic space.
@@ -63,10 +56,15 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
 
         nlev_tlm = _extend_cl(nlev_t, lmax_len)
 
-        self.inoise_2  = _extend_cl(transf ** 2, lmax_len) * cli(nlev_tlm ** 2) * (180 * 60 / np.pi) ** 2
-        self.inoise_1  = _extend_cl(transf ** 1 ,lmax_len) * cli(nlev_tlm ** 2) * (180 * 60 / np.pi) ** 2
-        self.transf    = _extend_cl(transf, lmax_len)
+        self.inoise_2 = _extend_cl(transf ** 2, lmax_len) * cli(nlev_tlm ** 2) * (180 * 60 / np.pi) ** 2
+        self.inoise_1 = _extend_cl(transf ** 1, lmax_len) * cli(nlev_tlm ** 2) * (180 * 60 / np.pi) ** 2
+        self.transf   = _extend_cl(transf, lmax_len)
 
+        if rescal is None:
+            rescal = np.ones(lmax_sol + 1, dtype=float)
+        assert rescal.size > lmax_sol and np.all(rescal >= 0.)
+        self.dorescal = np.any(rescal != 1.)
+        self.rescali = cli(rescal)
 
         self.verbose = verbose
         self.tim = timer(True, prefix='opfilt')
@@ -88,9 +86,13 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
         """
         lmax_unl = Alm.getlmax(tlm.size, self.mmax_sol)
         assert lmax_unl == self.lmax_sol, (lmax_unl, self.lmax_sol)
+        if self.dorescal:
+            almxfl(tlm, self.rescali, self.mmax_sol, True)
         tlmc = self.ffi.lensgclm(tlm, self.mmax_sol, 0, self.lmax_len, self.mmax_len)
         almxfl(tlmc, self.inoise_2, self.mmax_len, inplace=True)
         tlm[:] = self.ffi.lensgclm(tlmc, self.mmax_len, 0, self.lmax_sol, self.mmax_sol, backwards=True)
+        if self.dorescal:
+            almxfl(tlm, self.rescali, self.mmax_sol, True)
 
     def get_qlms(self, tlm_dat: np.ndarray, tlm_wf: np.ndarray, q_pbgeom: utils_scarf.pbdGeometry, alm_wf_leg2=None):
         """Get lensing generaliazed QE consistent with filter assumptions
@@ -104,7 +106,6 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
             All implementation signs are super-weird but end result should be correct...
 
         """
-        assert Alm.getlmax(tlm_dat.size, self.mmax_len) == self.lmax_len, (Alm.getlmax(tlm_dat.size, self.mmax_len), self.lmax_len)
         assert Alm.getlmax(tlm_wf.size, self.mmax_sol) == self.lmax_sol, (Alm.getlmax(tlm_wf.size, self.mmax_sol), self.lmax_sol)
         if alm_wf_leg2 is None:
             d1 = self._get_irestmap(tlm_dat, tlm_wf, q_pbgeom) * self._get_gtmap(tlm_wf, q_pbgeom)
@@ -116,6 +117,43 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
         fl = - np.sqrt(np.arange(self.ffi.lmax_dlm + 1, dtype=float) * np.arange(1, self.ffi.lmax_dlm + 2))
         almxfl(G, fl, self.ffi.mmax_dlm, True)
         almxfl(C, fl, self.ffi.mmax_dlm, True)
+        return G, C
+
+    def get_qlms_mf(self, mfkey, q_pbgeom:utils_scarf.pbdGeometry, mchain, phas=None, cls_filt:dict or None=None):
+        """Mean-field estimate using tricks of Carron Lewis appendix
+
+
+        """
+        if mfkey in [1]: # This should be B^t x, D dC D^t B^t Covi x, x random phases in alm space
+            if phas is None:
+                phas = synalm(np.ones(self.lmax_len + 1, dtype=float), self.lmax_len, self.mmax_len)
+            assert Alm.getlmax(phas.size, self.mmax_len) == self.lmax_len
+
+            soltn = np.zeros(Alm.getsize(self.lmax_sol, self.mmax_sol), dtype=complex)
+            mchain.solve(soltn, phas, dot_op=self.dot_op())
+
+            almxfl(phas,  self.transf, self.mmax_len, True)
+            #
+            assert 0, ' finish this'
+            repmap, impmap = q_pbgeom.geom.alm2map_spin(phas, 2, self.lmax_len, self.mmax_len, self.ffi.sht_tr, (-1., 1.))
+
+            Gs, Cs = self._get_gpmap([soltn, np.zeros_like(soltn)], 3, q_pbgeom)  # 2 pos.space maps
+            GC = (repmap - 1j * impmap) * (Gs + 1j * Cs)  # (-2 , +3)
+            Gs, Cs = self._get_gpmap([soltn, np.zeros_like(soltn)], 1, q_pbgeom)
+            GC -= (repmap + 1j * impmap) * (Gs - 1j * Cs)  # (+2 , -1)
+            del repmap, impmap, Gs, Cs
+        elif mfkey in [0]: # standard gQE, quite inefficient but simple
+            assert 0, 'not implemented'
+
+        else:
+            assert 0, mfkey + ' not implemented'
+        lmax_qlm = self.ffi.lmax_dlm
+        mmax_qlm = self.ffi.mmax_dlm
+        G, C = q_pbgeom.geom.map2alm_spin([GC.real, GC.imag], 1, lmax_qlm, mmax_qlm, self.ffi.sht_tr, (-1., 1.))
+        del GC
+        fl = - np.sqrt(np.arange(lmax_qlm + 1, dtype=float) * np.arange(1, lmax_qlm + 2))
+        almxfl(G, fl, mmax_qlm, True)
+        almxfl(C, fl, mmax_qlm, True)
         return G, C
 
     def _get_irestmap(self, tlm_dat:np.ndarray, tlm_wf:np.ndarray, q_pbgeom:utils_scarf.pbdGeometry):
@@ -138,7 +176,7 @@ class alm_filter_nlev_wl(opfilt_base.scarf_alm_filter_wl):
 
 
         """
-        assert  Alm.getlmax(tlm_wf.size, self.mmax_sol)== self.lmax_sol, ( Alm.getlmax(tlm_wf.size, self.mmax_sol), self.lmax_sol)
+        assert Alm.getlmax(tlm_wf.size, self.mmax_sol) == self.lmax_sol, ( Alm.getlmax(tlm_wf.size, self.mmax_sol), self.lmax_sol)
         fl = -np.sqrt(np.arange(self.lmax_sol + 1) * np.arange(1, self.lmax_sol + 2))
         ffi = self.ffi.change_geom(q_pbgeom) if q_pbgeom is not self.ffi.pbgeom else self.ffi
         return ffi.gclm2lenmap([almxfl(tlm_wf, fl, self.mmax_sol, False), np.zeros_like(tlm_wf)], self.mmax_sol, 1, False)
@@ -159,5 +197,5 @@ def calc_prep(tlm:np.ndarray, s_cls:dict, ninv_filt:alm_filter_nlev_wl):
     tlmc = np.copy(tlm)
     almxfl(tlmc, ninv_filt.inoise_1, ninv_filt.mmax_len, True)
     tlmc = ninv_filt.ffi.lensgclm(tlmc, ninv_filt.mmax_len, 0, ninv_filt.lmax_sol,ninv_filt.mmax_sol, backwards=True)
-    almxfl(tlmc, s_cls['tt'] > 0., ninv_filt.mmax_sol, True)
+    almxfl(tlmc, ninv_filt.rescali * (s_cls['tt'][ninv_filt.lmax_sol + 1] > 0.), ninv_filt.mmax_sol, True)
     return tlmc
