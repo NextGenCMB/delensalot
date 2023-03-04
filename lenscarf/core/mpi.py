a17dc316fd890d511a0b5a57b2a7cda4f192d7c4
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-03-04 13:11:11 +0100

    add bcast to non-mpi

--- lenscarf/core/mpi.py
+++ lenscarf/core/mpi.py
@@ -42,7 +42,8 @@ def check_MPI(func):
         return func(*args, **kwargs)
     return inner_function
     
-    
+def check_MPI_inline():
+    log.info("rank: {}, size: {}, name: {}".format(rank, size, name))
 
 verbose = True
 has_key = lambda key : key in os.environ.keys()
@@ -53,10 +54,16 @@ if not is_notebook() and cond4mpi4py:
 
     rank = MPI.COMM_WORLD.Get_rank()
     size = MPI.COMM_WORLD.Get_size()
-    comm = MPI.COMM_WORLD
-    send = MPI.COMM_WORLD.Send
-    receive = MPI.COMM_WORLD.Recv
     barrier = MPI.COMM_WORLD.Barrier
+    ANY_SOURCE = MPI.ANY_SOURCE
+    send = MPI.COMM_WORLD.send
+    receive = MPI.COMM_WORLD.recv
+    bcast = MPI.COMM_WORLD.bcast
+    status = MPI.Status
+    iprobe = MPI.COMM_WORLD.Iprobe
+    Ibcast = MPI.COMM_WORLD.Ibcast
+    Isend = MPI.COMM_WORLD.Isend
+    Ireceive = MPI.COMM_WORLD.Irecv
     finalize = MPI.Finalize
     name = "{} with {} cpus".format( platform.processor(),multiprocessing.cpu_count())
     log.info('mpi.py : setup OK, rank %s in %s' % (rank, size))
@@ -66,6 +73,7 @@ else:
     rank = 0
     size = 1
     barrier = lambda: -1
+    bcast = lambda _: 0
     finalize = lambda: -1
     receive = lambda val, src : 0
     send = lambda val, dst : 0
