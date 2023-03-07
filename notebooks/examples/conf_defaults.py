a115e2c1d0fd09890c65c6210ddcecafa8943b47
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-03-07 19:44:54 +0100

    minor

--- notebooks/examples/conf_defaults.py
+++ notebooks/examples/conf_defaults.py
@@ -6,5 +6,5 @@ from delensalot.lerepi.core.metamodel.dlensalot_mm import DLENSALOT_Model, DLENS
 
 dlensalot_model = DLENSALOT_Model(
         defaults_to='FS_CMB-S4_Pol',
-        job = DLENSALOT_Job(jobs = ["MAP_lensrec"])
+        job = DLENSALOT_Job(jobs = ["generate_sim", "MAP_lensrec"])
     )
