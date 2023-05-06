c0603597fc810ca2579c538ffa416a7f33a2eab0
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-05-06 12:10:42 +0200

    add author, and byebye fortran

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ with open("README.md", "r") as fh:
 
 def configuration(parent_package='', top_path=''):
     config = Configuration('', parent_package, top_path)
-    for ext in ['.f90', '.f95']:
-        modules = glob.glob('delensalot/fortran/*' + ext)
-        for modu in modules:
-            nam = modu.split('/')[-1].replace(ext, '')
-            config.add_extension('delensalot.fortran.' + nam, ['delensalot/fortran/%s'%nam + ext],
-                extra_link_args=['-lgomp'],libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w' , '-O3', '-ffast-math'])
     return config
 
 setup(
@@ -24,9 +18,9 @@ setup(
     data_files=[('delensalot/data/cls', ['delensalot/data/cls/FFP10_wdipole_lensedCls.dat',
                                 'delensalot/data/cls/FFP10_wdipole_lenspotentialCls.dat',
                                 'delensalot/data/cls/FFP10_wdipole_params.ini'])],
-    url='https://github.com/carronj/delensalot',
-    author='Julien Carron',
-    author_email='to.jcarron@gmail.com',
+    url='https://github.com/NextGenCMB/delensalot',
+    author='Julien Carron, Sebastian Belkner',
+    author_email='to.jcarron@gmail.com, to.sebastianbelkner@gmail.com',
     description='Iterative CMB lensing reconstruction on curved-sky',
     install_requires=['numpy', 'pyfftw', 'healpy', 'logdecorator', 'psutil', 'ducc0'], #removed mpi4py for travis tests
     requires=['numpy'],
