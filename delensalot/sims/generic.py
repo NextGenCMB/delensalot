8956eb73a2fd4360a711d2b44024e48fdcb62bd8
jcarron <to.jcarron@gmail.com>
2023-05-23 20:44:23 +0200

    generic sim epsilon to 1e-7

--- delensalot/sims/generic.py
+++ delensalot/sims/generic.py
@@ -52,7 +52,7 @@ class sims_cmb_len(object):
 
     """
     def __init__(self, lib_dir, lmax, cls_unl, lib_pha=None, offsets_plm=None, offsets_cmbunl=None,
-                 dlmax=1024, nside_lens=4096, epsilon=1e-5, nbands=8, cache_plm=True, verbose=True):
+                 dlmax=1024, nside_lens=4096, epsilon=1e-7, nbands=8, cache_plm=True, verbose=True):
 
         fields = _get_fields(cls_unl)
 
