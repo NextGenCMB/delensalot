ef5956d16d512ac8f9eac14570f3709f0f63ca3f
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-05-27 12:20:27 +0200

    add description

---
+++ delensalot/core/opfilt/__init__.py
@@ -0,0 +1,3 @@
+"""opfilt.__init__.py: These are QE and MAP filters, and include both isotropic (unmasked sky) and isotropic (masked sky) versions.
+The e/p/t/gpt refer to E-only/polarization/temperature/generalized-polarization-temperature
+"""
