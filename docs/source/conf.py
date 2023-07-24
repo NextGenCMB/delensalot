6c65ca792072b60d461f903b1860abecad3a01f4
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-07-24 20:24:56 +0200

    change paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ autodoc_mock_imports = ['plancklens', 'MSC', 'bicubic', 'mpi4py', 'attr', 'attrs
 
 # sys.path.insert(0, "./../")
 current_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(current_path, "../"))
+sys.path.insert(0, os.path.join(current_path, "../../"))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
