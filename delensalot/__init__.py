54321ad6f5608eaa299d81a7fc40ca9bfd9cb9ae
Sebastian Belkner <sebastianbelkner@gmail.com>
2025-03-09 08:19:46 -0400

    Update __init__.py
    Adds default scratch if none present

--- delensalot/__init__.py
+++ delensalot/__init__.py
@@ -1,2 +1,8 @@
+from pathlib import Path
 import os
+from os.path import join as opj
+
 os.environ['USE_PLANCKLENS_MPI'] = ""
+
+if 'SCRATCH' not in os.environ:
+    os.environ['SCRATCH'] = opj(Path(__file__).resolve().parent.parent, 'delensalot_temp')
