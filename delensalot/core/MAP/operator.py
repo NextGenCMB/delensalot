e0b066252d4f84ecb9d733339af5222a502a21cf
sebastian-belkner <sebastianbelkner@gmail.com>
2025-03-23 11:38:05 -0700

    minor stash

--- delensalot/core/MAP/operator.py
+++ delensalot/core/MAP/operator.py
@@ -161,6 +161,8 @@ class Lensing(Operator):
                 tlm = np.atleast_2d(self.ffi.lensgclm(obj[0], self.lm_max_in[1], 0, *self.lm_max_out, backwards=backwards, out_sht_mode='STANDARD')) if self.data_key in ['tt', 'tp'] else np.zeros(shape=(Alm.getsize(*self.lm_max_out)),dtype=complex)
                 print('lensing with', self.lm_max_in[1], 2, *self.lm_max_out)
                 print(self.ffi.__dict__)
+                print(np.atleast_2d(obj[1:]))
+                print('now comes lensing')
                 eblm = np.atleast_2d(self.ffi.lensgclm(np.atleast_2d(obj[1:]), self.lm_max_in[1], 2, *self.lm_max_out, backwards=backwards, out_sht_mode="GRAD_ONLY")) if self.data_key in ['p', 'ee', 'eb', 'bb', 'tp'] else np.zeros(shape=(1, Alm.getsize(*self.lm_max_out)),dtype=complex)
                 return np.array([tlm.squeeze(), *eblm, np.zeros_like(tlm.squeeze())])
             else:
