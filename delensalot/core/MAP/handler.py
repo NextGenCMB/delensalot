5ceb328163d542373bf6a7015c290be86e03ab98
sebastian-belkner <sebastianbelkner@gmail.com>
2025-03-23 12:28:45 -0700

    minor stash

--- delensalot/core/MAP/handler.py
+++ delensalot/core/MAP/handler.py
@@ -77,6 +77,7 @@ class Minimizer:
             if not self.use_QE_starting_point and it == 1:
                 for sec, val in est_prev.items():
                     est_prev[sec] = np.zeros_like(val,dtype=complex)
+            print('previous estimate:', est_prev)
             self.update_operator(est_prev)
             grad_tot = self.likelihood.get_likelihood_gradient(it)
             grad_tot = np.concatenate([np.ravel(arr) for arr in grad_tot])
--- delensalot/core/MAP/operator.py
+++ delensalot/core/MAP/operator.py
@@ -160,7 +160,7 @@ class Lensing(Operator):
             if adjoint and backwards:
                 tlm = np.atleast_2d(self.ffi.lensgclm(obj[0], self.lm_max_in[1], 0, *self.lm_max_out, backwards=backwards, out_sht_mode='STANDARD')) if self.data_key in ['tt', 'tp'] else np.zeros(shape=(Alm.getsize(*self.lm_max_out)),dtype=complex)
                 # print('lensing with', self.lm_max_in[1], 2, *self.lm_max_out)
-                # print(self.ffi.__dict__)
+                print(self.ffi.__dict__)
                 # print(np.atleast_2d(obj[1:]))
                 print('now comes lensing')
                 print(obj.dtype, self.ffi.dlm.dtype)
