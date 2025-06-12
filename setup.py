fb03ecb95cf0bd2c993cc4b5ce7fd13d33fa79d2
Louis Legrand <50632020+louisl3grand@users.noreply.github.com>
2025-06-12 09:17:50 +0000

    updates from copilot

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
+
 import setuptools
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
 import glob
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-def configuration(parent_package='', top_path=''):
-    config = Configuration('', parent_package, top_path)
-    return config
 
-setup(
+
+setuptools.setup(
     name='delensalot',
     version='0.1',
     packages=[
@@ -21,10 +19,13 @@ setup(
         'delensalot.utility',
         'delensalot.config', 'delensalot.config.default', 'delensalot.config.etc', 'delensalot.config.metamodel', 'delensalot.config.transformer', 'delensalot.config.validator',
         'delensalot.core', 'delensalot.core.cg', 'delensalot.core.decorator', 'delensalot.core.helper', 'delensalot.core.iterator', 'delensalot.core.ivf', 'delensalot.core.opfilt', 'delensalot.core.power',
-        'delensalot.sims',],
-    data_files=[('delensalot/data/cls', ['delensalot/data/cls/FFP10_wdipole_lensedCls.dat',
-                                'delensalot/data/cls/FFP10_wdipole_lenspotentialCls.dat',
-                                'delensalot/data/cls/FFP10_wdipole_params.ini'])],
+        'delensalot.sims',
+    ],
+    data_files=[('delensalot/data/cls', [
+        'delensalot/data/cls/FFP10_wdipole_lensedCls.dat',
+        'delensalot/data/cls/FFP10_wdipole_lenspotentialCls.dat',
+        'delensalot/data/cls/FFP10_wdipole_params.ini',
+    ])],
     url='https://github.com/NextGenCMB/delensalot',
     author='Julien Carron, Sebastian Belkner',
     author_email='to.jcarron@gmail.com, to.sebastianbelkner@gmail.com',
@@ -33,9 +34,9 @@ setup(
         'numpy',
         'logdecorator',
         'psutil',
-        'attrs'
+        'attrs',
     ],
-    requires=['numpy'],
     long_description=long_description,
-    configuration=configuration)
+    long_description_content_type='text/markdown',
+)
 
